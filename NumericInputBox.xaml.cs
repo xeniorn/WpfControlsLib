9b09cf925bb146241776d89d86e955a735ca617f
Juraj Ahel <juraj.ahel@gmail.com>
2021-03-16 13:10:09 +0100

    Add project files.

---
+++ NumericInputBox.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfControlsLib
+{
+    /// <summary>
+    /// Interaction logic for NumericInputBox.xaml
+    /// </summary>
+    public partial class NumericInputBox : UserControl
+    {
+        public static readonly DependencyProperty LabelProperty = DependencyProperty.Register("Label", typeof(string), typeof(NumericInputBox), new PropertyMetadata(default(string)));
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(double), typeof(NumericInputBox), new PropertyMetadata(default(double)));
+
+
+        public NumericInputBox()
+        {
+            InitializeComponent();
+        }
+
+        public string Label
+        {
+            get { return (string) GetValue(LabelProperty); }
+            set { SetValue(LabelProperty, value); }
+        }
+
+        public double Value
+        {
+            get { return (double) GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+    }
+}
