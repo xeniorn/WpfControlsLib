687d3c2fff5385d2c3325e402e0a384edcf7dd6c
Juraj Ahel <juraj.ahel@gmail.com>
2022-02-10 21:21:27 +0100

    add helper thing

---
+++ WpfHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfControlsLib
+{
+    public static class WpfHelper
+    {
+        public static string GetStringFromInputBox(object startValue, string prompt)
+        {
+            var aaa = new InputBoxPopup() {PromptText = prompt, Text = startValue.ToString()};
+
+            aaa.ShowDialog();
+
+            var text = aaa.Text;
+            
+            return text;
+        }
+
+        public static string GetStringFromInputBox(object startValue)
+        {
+            string defaultPrompt = "Input value:";
+            return GetStringFromInputBox(startValue, defaultPrompt);
+        }
+
+        public static string GetStringFromInputBox()
+        {
+            string defaultPrompt = "Input value:";
+            string defaultStartValue = "";
+            return GetStringFromInputBox(defaultStartValue, defaultPrompt);
+        }
+
+    }
+}
