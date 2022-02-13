53b6d8ed153fbd6ff7b270bc6a1b0c745340b460
Juraj Ahel <juraj.ahel@gmail.com>
2022-02-13 01:02:59 +0100

    make number input smarter, allowing different input formats nuget 1.3

---
+++ NumberInputSmartSuperConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Security.AccessControl;
+using System.Text.RegularExpressions;
+using System.Windows.Data;
+using System.Windows.Navigation;
+
+namespace WpfControlsLib
+{
+    public class NumberInputSmartSuperConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var numericValue = (double)value;
+
+            return numericValue.ToString(culture);
+
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            
+            var stringValue = (string)value;
+
+            if (String.IsNullOrWhiteSpace(stringValue)) return (double)0;
+            
+            if (double.TryParse(stringValue, NumberStyles.Float, culture, 
+                    out var resultCurrentCulture))
+            {
+                return resultCurrentCulture;
+            }
+
+            if (double.TryParse(stringValue, NumberStyles.Float, CultureInfo.InvariantCulture,
+                    out var resultInvariantCulture))
+            {
+                return resultInvariantCulture;
+            }
+            
+            // didn't manage to convert
+            return null;
+
+        }
+    }
+}
--- NumberViewingSuperConverter.cs
+++ NumberViewingSuperConverter.cs
@@ -11,19 +11,19 @@ namespace WpfControlsLib
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
             var value = (double)values[0];
-            var parentObject = (NumericInputBox)values[1];
+            var parentNumericInputBox = (NumericInputBox)values[1];
             
             //if (!parentObject.ValueBox.IsFocused) return value;
 
-            switch (parentObject.RoundingMode)
+            switch (parentNumericInputBox.RoundingMode)
             {
                 case RoundingMode.Auto:
                 case RoundingMode.None:
                     return value.ToString();
                 case RoundingMode.DecimalPlaces:
-                    return StringFromFixedDecimalPlaces(value, parentObject.DecimalSpots);
+                    return StringFromFixedDecimalPlaces(value, parentNumericInputBox.DecimalSpots);
                 case RoundingMode.SignificantDigits:
-                    return StringFromFixedSignificantDigits(value, parentObject.SignificantDigits);
+                    return StringFromFixedSignificantDigits(value, parentNumericInputBox.SignificantDigits);
                 default:
                     return "";
             }
@@ -35,7 +35,7 @@ namespace WpfControlsLib
             throw new NotSupportedException();
         }
 
-        private string StringFromFixedSignificantDigits(double value, int parentObjectSignificantDigits)
+        private string StringFromFixedSignificantDigits(double value, int targetSignificantDigits)
         {
             var numericValue = value;
             
@@ -55,18 +55,18 @@ namespace WpfControlsLib
 
             var logScale = (int)Math.Floor(Math.Log10(Math.Abs(numericValue))) + 1;
             var scale = Math.Pow(10, logScale);
-            var finalVal = scale * Math.Round(numericValue / scale, parentObjectSignificantDigits);
+            var finalVal = scale * Math.Round(numericValue / scale, targetSignificantDigits);
 
 
-            if (parentObjectSignificantDigits > scale)
+            if (targetSignificantDigits > scale)
             {
                 if (numericValue < 1)
                 {
-                    return finalVal.ToString(CultureInfo.InvariantCulture);
+                    return finalVal.ToString();
                 }
 
-                var afterDecimal = parentObjectSignificantDigits - logScale;
-                var formatString = "#." + new string('0', afterDecimal);
+                var afterDecimal = targetSignificantDigits - logScale;
+                var formatString = "0." + new string('0', afterDecimal);
                 return finalVal.ToString(formatString);
             }
 
@@ -76,7 +76,7 @@ namespace WpfControlsLib
             }
         }
 
-        private string StringFromFixedDecimalPlaces(double value, int parentObjectDecimalSpots)
+        private string StringFromFixedDecimalPlaces(double value, int targetDecimalSpots)
         {
             var numericValue = value;
 
@@ -92,14 +92,15 @@ namespace WpfControlsLib
             //    return value;
             //}
 
-            if (parentObjectDecimalSpots > 0)
+            if (targetDecimalSpots > 0)
             {
-                var finalVal = Math.Round(numericValue, parentObjectDecimalSpots);
-                return finalVal.ToString();
+                var format = "0." + new string('0', targetDecimalSpots);
+                var finalVal = Math.Round(numericValue, targetDecimalSpots);
+                return finalVal.ToString(format);
             }
 
-            var factor = Math.Pow(10, parentObjectDecimalSpots);
-            var reverseFactor = (int)Math.Pow(10, -1 * parentObjectDecimalSpots);
+            var factor = Math.Pow(10, targetDecimalSpots);
+            var reverseFactor = (int)Math.Pow(10, -1 * targetDecimalSpots);
 
             return ((int)(Math.Round(numericValue * factor, 0) * reverseFactor)).ToString();
         }
