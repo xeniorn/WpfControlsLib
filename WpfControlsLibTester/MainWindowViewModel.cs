37c2599b23efa2a3d694ca96e67f38c544993ec3
Juraj Ahel <juraj.ahel@gmail.com>
2024-04-29 23:53:19 +0200

    fix for multiple versions of .net

--- WpfControlsLibTester/MainWindowViewModel.cs
+++ WpfControlsLibTester/MainWindowViewModel.cs
@@ -1,33 +1,34 @@
 using PropertyChanged;
 
-namespace WpfControlsLibTester;
-
-[AddINotifyPropertyChangedInterface]
-public class MainWindowViewModel
+namespace WpfControlsLibTester
 {
-
-    public MainWindowViewModel()
+    [AddINotifyPropertyChangedInterface]
+    public class MainWindowViewModel
     {
-        OldValue = 5;
 
-        Val_AutoRound = 12345.12345;
-        Val_Decimal      = 12345.12345  ;
-Val_Sig          = 12345.12345  ;
-Val_NoRound      = 12345.12345  ;
-Val_Int          = 12345.12345  ;
-Val_Text         = 12345.12345  ;
+        public MainWindowViewModel()
+        {
+            OldValue = 5;
+
+            Val_AutoRound = 12345.12345;
+            Val_Decimal      = 12345.12345  ;
+            Val_Sig          = 12345.12345  ;
+            Val_NoRound      = 12345.12345  ;
+            Val_Int          = 12345.12345  ;
+            Val_Text         = 12345.12345  ;
 
 
+        }
+
+        public double OldValue { get; set; }
+
+        public double Val_AutoRound { get; set; }
+        public double Val_Decimal { get; set; }
+        public double Val_Sig { get; set; }
+        public double Val_NoRound { get; set; }
+        public double Val_Int { get; set; }
+        public double Val_Text { get; set; }
+
+        public object TextFromPopup { get; set; }
     }
-
-    public double OldValue { get; set; }
-
-    public double Val_AutoRound { get; set; }
-    public double Val_Decimal { get; set; }
-    public double Val_Sig { get; set; }
-    public double Val_NoRound { get; set; }
-    public double Val_Int { get; set; }
-    public double Val_Text { get; set; }
-
-    public object TextFromPopup { get; set; }
 }
